f0c28b4ea5ac217a2e01fffd572f302e9f2eaccf
whycantiputnothing <happytreefriends001@yahoo.com>
2017-11-12 13:55:38 -0800

    look and feel to windows and mac if sinstalled

--- src/Driver.java
+++ src/Driver.java
@@ -16,14 +16,18 @@ public class Driver {
      * @param args
      */
     public static void main(String args[]) {
-        /* Set the Metal look and feel */
+        /* Set the look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Metal".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+                else if ("Aqua".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
