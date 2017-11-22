52437e8b26b6dc9989595fcfb0ebda8af6a720f4
Brandon Nguyen <happytreefriends001@yahoo.com>
2017-11-21 18:05:40 -0800

    last updated time

--- src/MiniTwitter/User.java
+++ src/MiniTwitter/User.java
@@ -1,6 +1,5 @@
 package MiniTwitter;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,13 +8,12 @@ import java.util.List;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Brandon
  */
 public class User implements Subject, Observer, UserMember {
-    
+
     private String uniqueID;
     private List<User> followers;
     private List<User> followings;
@@ -30,7 +28,7 @@ public class User implements Subject, Observer, UserMember {
      *
      * @param ID String for identification
      */
-    public User(String ID){
+    public User(String ID) {
         uniqueID = ID;
         followers = new ArrayList();
         followings = new ArrayList();
@@ -39,25 +37,27 @@ public class User implements Subject, Observer, UserMember {
         creationTime = System.currentTimeMillis();
         lastUpdateTime = creationTime;
     }
-    
+
     /**
-     * 
+     *
      * @param s String to be added to newsFeed
      */
     public void addNewsFeed(String s) {
         newsFeed.add(uniqueID + ": " + s);
+        setLastUpdateTime(System.currentTimeMillis());
         NotifyObservers();
     }
-    
+
     /**
-     * used when notifying observers, else every observer would
-     * get duplicate copies of messages
-     * @param s 
+     * used when notifying observers, else every observer would get duplicate
+     * copies of messages
+     *
+     * @param s
      */
     public void addFeed(String s) {
         newsFeed.add(s);
     }
-    
+
     /**
      *
      * @param u user to be added to followers
@@ -65,7 +65,7 @@ public class User implements Subject, Observer, UserMember {
     public void addFollowers(User u) {
         followers.add(u);
     }
-    
+
     /**
      *
      * @param u user to be added to followings
@@ -137,8 +137,8 @@ public class User implements Subject, Observer, UserMember {
     public void setNewsFeed(List<String> newsFeed) {
         this.newsFeed = newsFeed;
     }
-    
-     public long getCreationTime() {
+
+    public long getCreationTime() {
         return creationTime;
     }
 
@@ -180,29 +180,31 @@ public class User implements Subject, Observer, UserMember {
     @Override
     public void update(String s) {
         addFeed(s);
-        }
-    
+        setLastUpdateTime(System.currentTimeMillis());
+    }
+
     /**
      *
      */
-    public void printFeed(){
-        for(String s : newsFeed) {
+    public void printFeed() {
+        for (String s : newsFeed) {
             System.out.println(s);
         }
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return getUniqueID();
     }
 
     /**
      * used to determine icon in jtree
+     *
      * @param userGroupVisitor
      */
     @Override
     public void accept(UserMemberVisitor userGroupVisitor) {
         userGroupVisitor.visit(this);
     }
-    
+
 }
